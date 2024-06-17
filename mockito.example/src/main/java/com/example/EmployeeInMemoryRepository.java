ecb2e0fbe8d0cfa44d7889b423eb4766bb056994
rajesh-upadhyay-revature <rajesh.upadhyay@revature.com>
2024-06-17 11:00:41 -0400

    first commit

---
+++ mockito.example/src/main/java/com/example/EmployeeInMemoryRepository.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class EmployeeInMemoryRepository implements EmployeeRepository {
+
+	private List<Employee> employees;
+
+	public EmployeeInMemoryRepository(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		return employees;
+	}
+
+	@Override
+	public Employee save(Employee employee) {
+		ListIterator<Employee> listIterator = employees.listIterator();
+		while (listIterator.hasNext()) {
+			if (listIterator.next().getId().equals(employee.getId())) {
+				listIterator.set(employee);
+				return employee;
+			}
+		}
+		employees.add(employee);
+		return employee;
+	}
+
+}
